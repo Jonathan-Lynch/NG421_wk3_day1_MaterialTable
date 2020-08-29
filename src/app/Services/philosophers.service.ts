695e2a540171a49a2026ee5aa9cf8b4747ea522d
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-08-29 07:53:56 -0500

    made service

--- src/app/Services/philosophers.service.ts
+++ src/app/Services/philosophers.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import { IPhilosophers } from '../interfaces/iphilosophers';
 import { PHILOSOPHERS_OF_HISTORY } from '../data/philosophers-data';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class PhilosophersService {
+  providedIn: 'root'
+})
+export class PhilosophersService {
 
-    members: IPhilosophers[] = [];
-  
-    constructor() {
-      this.members = PHILOSOPHERS_OF_HISTORY;
-    }
-  
-    getMembers(): IPhilosophers[] {
-      return this.members;
-    }
+  philosopherThinkers: IPhilosophers[] = [];
+
+  constructor() { 
+    this.philosopherThinkers = PHILOSOPHERS_OF_HISTORY;
   }
+
+  getPhilosophers(): IPhilosophers[] {
+    return this.philosopherThinkers;
+  }
+}
