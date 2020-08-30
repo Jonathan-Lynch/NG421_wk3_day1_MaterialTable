888f78159f661d28bd5cab75345f6d8b5510a5ad
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-08-29 22:12:21 -0500

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { IPhilosophers } from '../app/interfaces/iphilosophers';
+import { PhilosophersService } from '../app/services/philosophers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MaterialTable';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'areaLived', 'ageOfDeath', 'areTheyChristian', 'sumOfBeliefs', 'birthDate'];
+  dataSource: MatTableDataSource<IPhilosophers>;
+
+  constructor(private PhilosophersService: PhilosophersService){}
+
+  ngOnInit(): void {}
 }
