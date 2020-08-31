7f8c345f2bff7bf4e7fe2b194b514186bae388d3
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-08-30 20:01:35 -0500

    mat sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { IPhilosophers } from '../app/interfaces/iphilosophers';
 import { PhilosophersService } from '../app/services/philosophers.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,11 @@ import { PhilosophersService } from '../app/services/philosophers.service';
 export class AppComponent implements OnInit {
   displayedColumns: string[] = ['name', 'areaLived', 'ageOfDeath', 'areTheyChristian', 'sumOfBeliefs', 'birthDate'];
   dataSource: MatTableDataSource<IPhilosophers>;
-
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   constructor(private PhilosophersService: PhilosophersService){}
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource(this.PhilosophersService.getPhilosophers());
+    this.dataSource.sort = this.sort;
   }
 }
